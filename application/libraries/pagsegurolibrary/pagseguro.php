1027e66bf4efc3069e3886491050972de6984a87
Gabriel Pedro <gpedro831@gmail.com>
2014-04-17 14:00:52 -0400

    Fix'd indenting

--- application/libraries/pagsegurolibrary/pagseguro.php
+++ application/libraries/pagsegurolibrary/pagseguro.php
@@ -23,28 +23,28 @@ class Pagseguro
 	{
 		return new PagSeguroAccountCredentials(self::$pgEmail, self::$pgToken);
 	}
-  
+
 	private function formatNumber($number,$n = 4) {
-	    return str_pad((int) $number,$n,"0",STR_PAD_LEFT);
+		return str_pad((int) $number,$n,"0",STR_PAD_LEFT);
 	}
-	
+
 	private function getNotificationType($type)
 	{
 		$notificationType = new PagSeguroNotificationType($type);
-        $strType = $notificationType->getTypeFromValue();
+		$strType = $notificationType->getTypeFromValue();
 
-        return $strType;
+		return $strType;
 	}
 
-    private static function transactionNotification($notificationCode)
-    {
-        try {
-            return PagSeguroNotificationService::checkTransaction(self::getCredentials(), $notificationCode);
-        } catch (PagSeguroServiceException $e) {
-            die($e->getMessage());
-        }
+	private static function transactionNotification($notificationCode)
+	{
+		try {
+			return PagSeguroNotificationService::checkTransaction(self::getCredentials(), $notificationCode);
+		} catch (PagSeguroServiceException $e) {
+			die($e->getMessage());
+		}
 
-    }
+	}
 
 	public static function getRetorno($code, $type)
 	{
@@ -52,16 +52,16 @@ class Pagseguro
 		if($code && $type)
 		{
 			$notificationType = new PagSeguroNotificationType($type);
-            $strType = $notificationType->getTypeFromValue();
+			$strType = $notificationType->getTypeFromValue();
 
-            switch ($strType) {
-                case 'TRANSACTION':
-                	return self::transactionNotification($code);
-                break;
+			switch ($strType) {
+				case 'TRANSACTION':
+				return self::transactionNotification($code);
+				break;
 
-                default:
-                    LogPagSeguro::error("Unknown notification type [" . $notificationType->getValue() . "]");
-					show_error('PagSeguroLibrary: Unknown notification type [' . $notificationType->getValue() . ']');
+				default:
+				LogPagSeguro::error("Unknown notification type [" . $notificationType->getValue() . "]");
+				show_error('PagSeguroLibrary: Unknown notification type [' . $notificationType->getValue() . ']');
 				break;
 			}
 		}
@@ -77,13 +77,13 @@ class Pagseguro
 		$requestPayment = new PagSeguroPaymentRequest();
 
 		$requestPayment->setCurrency($currency);
-		
+
 		$itemId = 1;
-	    foreach($items as $item)
-	    {
-	      $item['id'] = $this->formatNumber($itemId++);
-	      $requestPayment->addItem($item);
-	    }
+		foreach($items as $item)
+		{
+			$item['id'] = $this->formatNumber($itemId++);
+			$requestPayment->addItem($item);
+		}
 
 		$requestPayment->setReference($reference);
 
@@ -94,32 +94,32 @@ class Pagseguro
 		$requestPayment->setShippingType($shipping['frete']);
 		$requestPayment->setShippingCost(0);
 		$requestPayment->setShippingAddress(
-				$shipping['cep'],
-				$shipping['rua'],
-				$shipping['numero'],
-				$shipping['complemento'],
-				$shipping['bairro'],
-				$shipping['cidade'],
-				$shipping['estado'],
-				$shipping['pais']
+			$shipping['cep'],
+			$shipping['rua'],
+			$shipping['numero'],
+			$shipping['complemento'],
+			$shipping['bairro'],
+			$shipping['cidade'],
+			$shipping['estado'],
+			$shipping['pais']
 			);
 
-		}
-		
-		$requestPayment->setRedirectURL($redirect);
-		$requestPayment->setMaxAge(86400 * 3);
-
-		try
-		{
-			return $requestPayment->register($this->getCredentials());
-		}
-		catch (PagSeguroServiceException $e)
-		{
-			show_error('PagSeguroLibrary: '. $e->getMessage());	
-		}
-
 	}
+
+	$requestPayment->setRedirectURL($redirect);
+	$requestPayment->setMaxAge(86400 * 3);
+
+	try
+	{
+		return $requestPayment->register($this->getCredentials());
+	}
+	catch (PagSeguroServiceException $e)
+	{
+		show_error('PagSeguroLibrary: '. $e->getMessage());	
+	}
+
 }
+
 
 /* End of file pagseguro.php */
 /* Location: ./application/libraries/pagseguro.php */
